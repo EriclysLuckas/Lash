642f7fffcc89627d8facf9a6d1dc2b8a520534bf
Ericlys <107273056+EriclysLuckas@users.noreply.github.com>
2024-11-06 11:36:47 -0300

    att

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import HeaderLash from "../src/components/HeaderLash/HeaderLash";
 import BannersHeader from "../src/components/BannersHeader/BannersHeader.jsx";
-import styleApp from "./app.module.css"
+import styleApp from "../src/App.module.css"
 import InfoRange from "../src/components/InfoRange/InfoRange"
 import TypeLashs from "../src/components/TypeLashs/TypeLashs"
 import GalleryLashs from './components/GalleryLashs/GalleryLashs';
