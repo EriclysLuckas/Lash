190146f92d806641403b0b6814f5c4ed4642642b
Ericlys <107273056+EriclysLuckas@users.noreply.github.com>
2024-11-06 11:47:24 -0300

    att

--- src/components/HeaderLash/HeaderLash.jsx
+++ src/components/HeaderLash/HeaderLash.jsx
@@ -2,20 +2,10 @@ import styleHeader from "../HeaderLash/HeaderLash.module.css";
 import { IoMenu, IoCloseSharp } from "react-icons/io5";
 import { useState, useEffect } from "react";
 
+
 export default function HeaderLash() {
   const [isOpen, setIsOpen] = useState(false); 
   const [isMobile, setIsMobile] = useState(false);
-
-
-  
-
-
-
-
-
-
-
-
 
 
   const checkMobile = () => {
@@ -67,7 +57,7 @@ export default function HeaderLash() {
         </div>
         <div className={styleHeader.logoLash}>
           <figure>
-            <img src="../img/logo.png" alt="" />
+            <img src="../../../img/logo.png" alt="" />
           </figure>
         </div>
       </div>
