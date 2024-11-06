cab5778904bc9b8333e38bafe41c8b3277cc8a1f
Ericlys <107273056+EriclysLuckas@users.noreply.github.com>
2024-11-06 15:08:53 -0300

    att

--- src/components/FooterComponent/FooterComponent.jsx
+++ src/components/FooterComponent/FooterComponent.jsx
@@ -6,7 +6,7 @@ export default function FooterComponent() {
   return (
     <section className={styleFooter.sectionFooter} id = "contatos">
       <div className={styleFooter.logoFooter}>
-        <img src="../img/logo.png" alt="" />
+        <img src="https://raw.githubusercontent.com/EriclysLuckas/Lash/refs/heads/main/img/logo.png" alt="" />
       </div>
       <div className={styleFooter.contentFooter}>
         <div className={styleFooter.linksIcons}>
@@ -20,7 +20,7 @@ export default function FooterComponent() {
         </div>
         <FormSite />
 
-        <div className={styleFooter.imgProfissional}> <img src="../img/profissional.png" alt="" />
+        <div className={styleFooter.imgProfissional}> <img src="https://raw.githubusercontent.com/EriclysLuckas/Lash/refs/heads/main/img/profissional.png" alt="" />
         </div>
       </div >
 
--- src/components/IconsFooter/IconsFooter.jsx
+++ src/components/IconsFooter/IconsFooter.jsx
@@ -6,7 +6,7 @@ export default function IconFooter() {
             <div className={styleFooter.groupIcon}>
 
               <div className={styleFooter.iconFooter}>
-                <a href="https://www.instagram.com/" target="_blank"> <img src="../img/links/instagram.png" alt="Instagram" /></a>
+                <a href="https://www.instagram.com/" target="_blank"> <img src="https://raw.githubusercontent.com/EriclysLuckas/Lash/refs/heads/main/img/links/instagram.png" alt="Instagram" /></a>
               </div>
               
           </div>
@@ -14,17 +14,17 @@ export default function IconFooter() {
 
           <div className={styleFooter.groupIconWhatsapp}>
             <div className={styleFooter.iconFooter}>
-              <a href="https://web.whatsapp.com/%F0%9F%8C%90/pt_br" target="_blank"><img src="../img/links/whatsapp.png" alt="WhatsApp" /></a>
+              <a href="https://web.whatsapp.com/%F0%9F%8C%90/pt_br" target="_blank"><img src="https://raw.githubusercontent.com/EriclysLuckas/Lash/refs/heads/main/img/links/whatsapp.png" alt="WhatsApp" /></a>
             </div>
           </div>
           <div className={styleFooter.groupIconTelefone}>
             <div className={styleFooter.iconFooter}>
-              <a href="tel:+6494461709" target="_blank"></a><img src="../img/links/telefone.png" alt="Telefone" />
+              <a href="tel:+6494461709" target="_blank"></a><img src="https://raw.githubusercontent.com/EriclysLuckas/Lash/refs/heads/main/img/links/telefone.png" alt="Telefone" />
             </div>
           </div>
           <div className={styleFooter.groupIconEmail}>
             <div className={styleFooter.iconFooter}>
-              <img src="../img/links/email.png" alt="Email" />
+              <img src="https://raw.githubusercontent.com/EriclysLuckas/Lash/refs/heads/main/img/links/email.png" alt="Email" />
             </div>
           </div>
         </div>
